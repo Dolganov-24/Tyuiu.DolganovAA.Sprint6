ee1ae41e084b76ea253ce42c8ecbab915483bbf4
Tyuiu.DolganovAA@outlook.com <Tyuiu.DolganovAA@outlook.com>
2025-01-24 15:30:16 +0300

    Task7

---
+++ Tyuiu.DolganovAA.Sprint6.Task7.V29.Lib/DataService.cs
@@ -0,0 +1,30 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint6;
+namespace Tyuiu.DolganovAA.Sprint6.Task7.V29.Lib
+{
+    public class DataService : ISprint6Task7V29
+    {
+        public int[,] GetMatrix(string path)
+        {
+            string[] file = File.ReadAllLines(path);
+            int[,] matrix = new int[file.Length, file[0].Count(f => f == ';') + 1];
+
+            for (int i = 0; i < file.Length; i++)
+            {
+                string[] line = file[i].Split(';');
+
+                for (int j = 0; j < line.Length; j++)
+                {
+                    int num = Convert.ToInt32(line[j]);
+                    if (i == 4 && num >= -5 && num <= 10)
+                    {
+                        matrix[i, j] = 99;
+                        continue;
+                    }
+                    matrix[i, j] = num;
+                }
+            }
+
+            return matrix;
+        }
+    }
+}
---
+++ Tyuiu.DolganovAA.Sprint6.Task7.V29.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+namespace Tyuiu.DolganovAA.Sprint6.Task7.V29.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
