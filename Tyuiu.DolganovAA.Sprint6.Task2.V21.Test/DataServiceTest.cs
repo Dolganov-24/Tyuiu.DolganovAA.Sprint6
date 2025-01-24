fb58322740d60f1f1a9b2d36c9ae4888d3775516
Tyuiu.DolganovAA@outlook.com <Tyuiu.DolganovAA@outlook.com>
2025-01-24 13:03:43 +0300

    Task2

---
+++ Tyuiu.DolganovAA.Sprint6.Task2.V21.Test/DataServiceTest.cs
@@ -0,0 +1,12 @@
+using Tyuiu.DolganovAA.Sprint6.Task2.V21.Lib;
+namespace Tyuiu.DolganovAA.Sprint6.Task2.V21.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Tyuiu.DolganovAA.Sprint6.Task2.V21/FormMain.cs
@@ -0,0 +1,15 @@
+namespace Tyuiu.DolganovAA.Sprint6.Task2.V21
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+
+        }
+    }
+}
